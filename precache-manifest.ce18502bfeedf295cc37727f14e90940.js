e0e8ad6a0aacd9679323285015403a7714ab0bec
Code Sandbox <hello@codesandbox.io>
2022-09-21 12:57:36 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ce18502bfeedf295cc37727f14e90940.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "029447c821c18132b72b",
+    "url": "https://anand980.github.io/csb-owex76/static/css/main.ba54d6fa.chunk.css"
+  },
+  {
+    "revision": "029447c821c18132b72b",
+    "url": "https://anand980.github.io/csb-owex76/static/js/main.029447c8.chunk.js"
+  },
+  {
+    "revision": "455d3a4716702d5cd61c",
+    "url": "https://anand980.github.io/csb-owex76/static/js/1.455d3a47.chunk.js"
+  },
+  {
+    "revision": "0150dbb113c6bc12a442",
+    "url": "https://anand980.github.io/csb-owex76/static/js/runtime~main.0150dbb1.js"
+  },
+  {
+    "revision": "f0cde7e3d792d54b8837649092a656eb",
+    "url": "https://anand980.github.io/csb-owex76/index.html"
+  }
+];
